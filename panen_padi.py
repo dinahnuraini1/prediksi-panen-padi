f609f6bd53e2bffe55d46b8b8765ac528cfd4d8c
dinahnuraini <112395290+dinahnuraini1@users.noreply.github.com>
2025-06-30 09:02:02 +0700

    Update panen_padi.py

--- panen_padi.py
+++ panen_padi.py
@@ -237,8 +237,8 @@ def main():
                             model_data = pickle.load(f)
                         model_rf = model_data.get("model")
                         params = params.get("params", {})
-                        mape_train = params.get("mape_train")
-                        mape_test = params.get("mape_test")
+                        mape_train = model_data.get("mape_train")
+                        mape_test = model_data.get("mape_test")
 
                         if model_rf and mape_train is not None and mape_test is not None:
                             # Tampilkan parameter model dalam input field yang tidak bisa diubah (read-only)
