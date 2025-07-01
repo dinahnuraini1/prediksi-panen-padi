334ad1e39060ee556bfc67652caf563ca578e05d
dinahnuraini <112395290+dinahnuraini1@users.noreply.github.com>
2025-07-01 18:23:14 +0700

    Update panen_padi.py

--- panen_padi.py
+++ panen_padi.py
@@ -529,7 +529,7 @@ def main():
                         y = st.session_state["y"]
                         scaler_y = st.session_state["scaler_y"]
 
-                        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+                        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size1=test_size1, random_state=42)
 
                         # Batasan parameter RF
                         bounds = {
